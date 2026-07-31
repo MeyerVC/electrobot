d8e6311fd24af84cbf52fdd34a40d5d0a8ecd4e2
MeyerVC <meyer.velsaco@uab.edu.bo>
2026-07-30 23:28:33 -0400

    sitio interactivo

--- script.js
+++ script.js
@@ -15,3 +15,42 @@ function alternarmodo() {
 
 botonmodo.addEventListener("click", alternarmodo);
 
+//boton hamburgesa
+const btnHamburger = document.querySelector("#btn-hamburger");
+const nav = document.querySelector("nav");
+
+function alternarMenu() {
+    nav.classList.toggle("activo");
+}
+
+btnHamburger.addEventListener("click", alternarMenu);
+
+/*
+const btnHamburger = document.querySelector("#btn-hamburger");
+const nav = document.querySelector("nav");
+
+function alternarMenu() {
+    if (nav.classList.contains("activo")) {
+        nav.classList.remove("activo");
+    } else {
+        nav.classList.add("activo");
+    }
+}
+
+btnHamburger.addEventListener("click", alternarMenu);
+*/
+
+// Contador del carrito
+const btnAñadir = document.querySelector("#añadir-prod");
+const contadorCarrito = document.querySelector("#contador-carrito");
+let contador = 0;
+
+function incrementarCarrito() {
+    contador = contador + 1;
+    contadorCarrito.textContent = contador;
+}
+
+btnAñadir.addEventListener("click", incrementarCarrito);
+
+
+
